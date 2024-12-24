116b0aac0b73c439de97342db7e0d9c448d2ba9d
quangpham-dev <quangpn.developer@gmail.com>
2024-12-24 17:15:26 +0700

    clean up

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,36 +1,44 @@
-import type { AuthContext } from "@/context/auth-context"
-import type { QueryClient } from "@tanstack/react-query"
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { Outlet, createRootRouteWithContext, redirect } from "@tanstack/react-router"
-import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import type { AuthContext } from "@/context/auth-context";
+import type { QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  Outlet,
+  createRootRouteWithContext,
+  redirect
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 interface RootRouterContext {
-	auth: AuthContext
-	queryClient: QueryClient
+  auth: AuthContext;
+  queryClient: QueryClient;
 }
 
 export const Route = createRootRouteWithContext<RootRouterContext>()({
-	component: RootComponent,
-	beforeLoad: async ({ context }) => {
-		const { auth } = context
-		const isAuthenticated = auth.isAuthenticated
+  component: RootComponent,
+  beforeLoad: async ({ context }) => {
+    const { auth } = context;
+    const isAuthenticated = auth.isAuthenticated;
 
-		// Check if the user is trying to access the root path
-		if (window.location.pathname === "/") {
-			if (isAuthenticated) {
-				throw redirect({ to: "/dashboard" })
-			}
-			throw redirect({ to: "/login" })
-		}
-	},
-})
+    // Check if the user is trying to access the root path
+    if (window.location.pathname === "/") {
+      if (isAuthenticated) {
+        throw redirect({ to: "/dashboard" });
+      }
+      throw redirect({ to: "/login" });
+    }
+  }
+});
 
 function RootComponent() {
-	return (
-		<>
-			<Outlet />
-			<ReactQueryDevtools buttonPosition="top-right" />
-			<TanStackRouterDevtools position="bottom-right" />
-		</>
-	)
+  return (
+    <>
+      <Outlet />
+      {import.meta.env.VITE_NODE_ENV === "development" && (
+        <>
+          <ReactQueryDevtools buttonPosition="top-right" />
+          <TanStackRouterDevtools position="bottom-right" />
+        </>
+      )}
+    </>
+  );
 }
--- tests/setup-tests.ts
+++ tests/setup-tests.ts
@@ -1,18 +1,11 @@
-import "@testing-library/jest-dom"
-import { cleanup } from "@testing-library/react"
-import { afterEach, beforeEach, vi } from "vitest"
+import "@testing-library/jest-dom";
+import { cleanup } from "@testing-library/react";
+import { afterEach, beforeEach, vi } from "vitest";
 
 beforeEach(() => {
-	vi.resetAllMocks()
-
-	vi.mock("next/font/local", () => ({
-		__esModule: true,
-		default: () => ({
-			style: { fontFamily: "mocked" },
-		}),
-	}))
-})
+  vi.resetAllMocks();
+});
 
 afterEach(() => {
-	cleanup()
-})
+  cleanup();
+});
